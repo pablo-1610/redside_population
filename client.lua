fba34c57ec541a1f3e8022657f71603a4e690454
Pablo Zapata <42814853+PABLO-1610@users.noreply.github.com>
2020-08-01 00:53:00 +0300

    Modifying client & config

--- client.lua
+++ client.lua
@@ -9,10 +9,15 @@ Citizen.CreateThread(function()
             local pedModel = GetHashKey(pedValue.model)
             RequestModel(pedModel)
             while not HasModelLoaded(pedModel) do Citizen.Wait(10) end
-            local ped = CreatePed_(9,pedModel,ped.pos.x,ped.pos.y,ped.pos.z,ped.pos.heading,false,true)
+
+            local pos = groundVector(ped.pos.x,ped.pos.y,ped.pos.z)
+
+            local ped = CreatePed_(9,pedModel,pos.x,pos.y,pos.z,ped.pos.heading,false,true)
         end
     end
-
-
-
 end)
+
+function groundVector(x,y,z) 
+    local _,groundZ,_ = GetGroundZAndNormalFor_3dCoord(pos.x,pos.y,pos.z)
+    return vector3(x,y,groundZ)
+end
--- config.lua
+++ config.lua
@@ -3,8 +3,8 @@ populationConfig = {
         [1] = { -- Groupe place des cubes
             filter = {"M_CHIC"},
             peds = {
-                {pos = groundVector(270.5746, -834.017, 29.28668),heading = 161.89764404296},
-                {pos = groundVector(275.606, -833.079, 29.28406),heading = 214.03770446778},
+                {pos = vector3(270.5746, -834.017, 29.28668),heading = 161.89764404296},
+                {pos = vector3(275.606, -833.079, 29.28406),heading = 214.03770446778},
             },
         },
     },
